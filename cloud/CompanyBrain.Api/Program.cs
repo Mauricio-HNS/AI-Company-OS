de70a7e110bac6d46e4ca67c441e764d5d57b8bc
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2026-09-21 16:28:11 +0200

    feat: register advanced agenda operations

--- cloud/CompanyBrain.Api/Program.cs
+++ cloud/CompanyBrain.Api/Program.cs
@@ -15,6 +15,7 @@ builder.Services.AddSingleton<CompanyMasterDataStore>();
 builder.Services.AddSingleton<CompanySalesOrderStore>();
 builder.Services.AddSingleton<CompanyInventoryStore>();
 builder.Services.AddSingleton<CompanyAgendaStore>();
+builder.Services.AddSingleton<CompanyAgendaAdvancedStore>();
 builder.Services.AddSingleton<BrainProcessor>();
 builder.Services.AddSingleton<BrainDecisionEngine>();
 builder.Services.AddSingleton<AgentRegistry>();
@@ -60,6 +61,7 @@ app.MapCompanyMasterDataApi();
 app.MapCompanySalesOrdersApi();
 app.MapCompanyInventoryApi();
 app.MapCompanyAgendaApi();
+app.MapCompanyAgendaAdvancedApi();
 
 static bool HasBrainAdminKey(HttpRequest request, IConfiguration configuration)
 {
